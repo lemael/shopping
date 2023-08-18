9955a101dadb805d9f81f84dcada3973cd1ef45b
Your Name <fosso@integreat-app.de>
2023-08-18 21:30:01 +0200

    Declaration of the object Itemcard and add  home page with a list of Item Card objects

---
+++ src/data.js
@@ -0,0 +1,32 @@
+import img1 from './images/img1.png';
+import img2 from './images/img2.png';
+import img3 from './images/img3.png';
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: img1,
+            title: 'Saftige Trauben',
+            desc: '',
+            preis: 46,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Rote Wassermelone',
+            desc: '',
+            preis: 56,
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Saftige Orange',
+            desc: '',
+            preis: 46,
+        }
+
+    ]
+};
+
+export default data;
